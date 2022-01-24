ffb520bd04e8b01e08b7dfba23dde6f268172f33
SUNEO 20 <97093992+Suneo20@users.noreply.github.com>
2022-01-24 12:08:05 +0700

    Update donasi.js

--- shop/donasi.js
+++ shop/donasi.js
@@ -8,7 +8,9 @@ const donasibot = () => {
 	ㅁ 0822 3039 7507
 3. DANA
 	ㅁ 0822 3039 7507
-}
+Sebelum melakukan pembayaran ada baiknya anda menghubungi owner terlebih dahulu!
+`
+	}
 
 exports.donasibot = donasibot
  
